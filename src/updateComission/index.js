f4f2986dbb984652cab5ef5649750cf84fe8e866
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-03 14:08:08 -0300

    implementando conferencia boleto baixado

--- src/updateComission/index.js
+++ src/updateComission/index.js
@@ -1,11 +1,30 @@
 const { db } = require('../firebase/index')
-const sendFirebase = require('./utils/pading')
+const arrayObject = require('@ziro/array-object')
+const sendFirebase = require('./utils/pending')
+const axios = require('axios')
 const moment = require('moment')
 
 require('dotenv').config()
 
 const updateComission = async (razao) => {
+        const config = {
+            method: 'GET',
+            url: process.env.SHEET_URL,
+            data: {
+                apiResource: 'values',
+                apiMethod: 'batchGet',
+                spreadsheetId: process.env.SHEET_ID_CHARGE,
+                ranges: ['BoletoBaixados!A:N']
+            },  
+            headers: {
+                'Authorization': process.env.SHEET_TOKEN,
+                'Content-Type': 'application/json',
+                'Origin': 'https://ziro.app'
+            }
+        }
         try {
+            const result = await axios(config)
+            const filtrado = arrayObject(result.data.valueRanges[0]).filter(item => item.fornecedor === razao)
             let arrayFirebase = []
             let arrayPending = []
             let queryPayments = db.collection('boleto-payments').where('fantasia', '==', razao.toUpperCase())
@@ -20,23 +39,29 @@ const updateComission = async (razao) => {
                     arrayPending.push({billets: doc.data().billets})
                 }
             })
+            let arrayPagos = []
+            arrayPending[0].billets.map(item => {
+                arrayPagos.push(...filtrado.filter((sheet) => {
+                    return item.boleto === sheet.boleto
+                }))
+            })
             const clientId = razao.toUpperCase();
-            const encodedData = `${Buffer.from(clientId).toString('base64')}${arrayFirebase.length+1}`;
-            console.log(encodedData)
+            const encodedData = `${Buffer.from(clientId).toString('base64')}${arrayFirebase.length+1}`
             if(arrayPending[0]){
-                const objeto = {
+                const updateObj = {
                     'fantasia': razao.toUpperCase(),
                     'status': 'Pagamento Realizado',
                     'date_payment': new Date(),
                     'counter': arrayFirebase.length+1,
-                    'billets': arrayPending[0].billets,
-                    'transactionZoopId': encodedData
+                    'transactionZoopId': encodedData,
+                    'payment_type': 'transfer',
+                    'billets': arrayPagos
                 }
-                await db.collection('boleto-payments').add(objeto)
-                console.log('\x1b[32m%s\x1b[0m','Atualização', objeto.billets)
+                await db.collection('boleto-payments').add(updateObj)
+                console.log('\x1b[32m%s\x1b[0m','Atualização', updateObj.billets.length)
                 await sendFirebase(razao)
-                console.log('\x1b[32m%s\x1b[0m','Fantasia', objeto.fantasia)
-                console.log('\x1b[32m%s\x1b[0m','Data do pagamento', moment(objeto.date_payment).format('DD/MM/YYYY'))
+                console.log('\x1b[32m%s\x1b[0m','Fantasia', updateObj.fantasia)
+                console.log('\x1b[32m%s\x1b[0m','Data do pagamento', moment(updateObj.date_payment).format('DD/MM/YYYY'))
                 console.log('\x1b[32m%s\x1b[0m','Status de pagamento atualizado com sucesso')
                 process.exit(0)  
             }else{
